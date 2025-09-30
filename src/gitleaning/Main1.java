3a74412db17f78921b08e82147fbc1a77eff97ea
tqha205 <tqha805@gmail.com>
2025-09-30 18:00:02 +0700

    ha0805 commit 4

--- src/gitleaning/Main1.java
+++ src/gitleaning/Main1.java
@@ -3,13 +3,9 @@ package gitleaning;
 public class Main1 {
     public static void main(String[] args) {
         int a = 0;
+        int d = 0;
         int b = 0;
         System.out.println("Hello");
         int c = 0;
-        int d = 0;
-        System.out.println("Hello");
-        int e = 0;
-        int g = 0;
-        System.out.println("Hello");
     }
 }
